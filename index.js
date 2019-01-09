53d00f2f9b59d42fc940dc58891fd9ad6e2b7076
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-01-09 19:58:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // write your code below!
 
 var name = "Pamyk"
-var height = "76"
+var height = 76
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height+1)
+console.log(parseInt(height)+1)
 
